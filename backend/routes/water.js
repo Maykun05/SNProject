c95ada66307985721b724b5f76120d1b3d232a2b
“Maykun05 <maykun0943158675@gmail.com>
2026-01-17 00:46:53 +0700

    commit

--- backend/routes/water.js
+++ backend/routes/water.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/waterController');
+// เปลี่ยนชื่อจาก controller เป็น waterController ให้ตรงกับด้านล่าง
+const waterController = require('../controllers/waterController'); 
 
-router.post('/', waterController.addWaterLog);
-router.get('/:userId', controller.getWaterToday);
+router.post('/log', waterController.addWaterLog); // เพิ่ม /log เพื่อให้ตรงกับ API Path
+router.get('/:userId', waterController.getWaterToday); 
 
 module.exports = router;
